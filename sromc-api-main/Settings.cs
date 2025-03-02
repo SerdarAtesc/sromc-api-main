018da98a07b4a418a14973bd853b717ec7cb362e
serdarates <serdareates@gmail.com>
2025-03-02 04:07:49 +0300

    Add initial model classes and configuration files

---
+++ sromc-api-main/Settings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SROMCapi
+{
+    public class Settings
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Domain = "sql.bsite.net\\MSSQL2016";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string DatabaseName = "corvustr_";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string DatabaseServer = "sql.bsite.net/MSSQL2016";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string DatabaseUsername = "corvustr_";
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string DatabasePassword = "hektor123";
+    }
+}
